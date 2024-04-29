6bae18219ba273cd48d6c43cd2c202bb056bf081
Shi3ofreniR <t40411304@yandex.ru>
2024-04-30 03:43:25 +0700

    Initial commit

---
+++ src/main/java/mod/nero/templatemodwithmixin/MainClass.java
@@ -0,0 +1,58 @@
+package mod.nero.templatemodwithmixin;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("templatemodwithmixin")
+public class MainClass
+{
+    public final String MOD_ID = "templatemodwithmixin";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MainClass()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+
+    }
+
+
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents
+    {
+
+    }
+}
---
+++ src/main/java/mod/nero/templatemodwithmixin/mixin/BlankMixin.java
@@ -0,0 +1,8 @@
+package mod.nero.templatemodwithmixin.mixin;
+
+import net.minecraft.sounds.SoundEvents;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SoundEvents.class)
+public abstract class BlankMixin {
+}
